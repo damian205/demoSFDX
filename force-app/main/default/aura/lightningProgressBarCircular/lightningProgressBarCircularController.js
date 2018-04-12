4c13b4f69a0c33b6df28249275915b14297185cc
DS\dkazior <dkazior@tractionondemand.com>
2018-04-11 21:22:56 -0400

    removed last time

--- force-app/main/default/aura/lightningProgressBarCircular/lightningProgressBarCircularController.js
+++
@@ -1,9 +0,0 @@
-({
-    doInit : function(component, event, helper) {
-        helper.doInit(component, event, helper) ;
-    },
-    refreshProgressBar : function(component, event, helper) {
-        console.log('inside refreshProgressBar');
-        helper.doInit(component, event, helper);
-    }
-})
